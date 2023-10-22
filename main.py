738fb9805ab4e076d845ada294330ef9b2f622fc
Nakib00 <2022142@iub.edu.bd>
2023-10-22 15:39:40 +0600

    up

--- main.py
+++ main.py
@@ -35,107 +35,136 @@ def take_commend():
 def run_urro():
     commend = take_commend()
     if 'time' in commend:
-        time = datetime.datetime.now().strftime('%H:%M %p')
-        print(time)
-        talk('Current time is' + time)
+        time_handler()
     elif 'play' in commend:
-        song = commend.replace('play', '')
-        talk("playing" + song)
-        pywhatkit.playonyt(song)
+        play_handler(commend)
     elif 'tell me about' in commend:
-        try:
-            look_for = commend.replace('tell me about','')
-            about = wikipedia.summary(look_for, 2)
-            print(about)
-            talk(about)
-        except:
-            pt = 'Not found in Wikipedia'
-            print(pt)
-            talk(pt)
+        wikipedia_handler(commend)
     elif 'department' in commend:
-            if ('department' and 'how') in commend:
-                pt = depermant_info['department']
-                print(pt)
-                talk(pt)
-            elif 'cse department' in commend:
-                pt = depermant_info['cse']
-                print(pt)
-                talk(pt)
-            elif 'bba department' in commend:
-                pt = depermant_info['bba']
-                print(pt)
-                talk(pt)
-            elif 'pharmacy department' in commend:
-                pt = depermant_info['pharmacy']
-                print(pt)
-                talk(pt)
+        department_handler(commend)
     elif 'admission' in commend:
-            if 'graduate' in commend:
-                pt = admission_info['graduate']
-                print(pt)
-                talk(pt)
-            else:
-                pt = admission_info['Undergraduate']
-                print(pt)
-                talk(pt)
+        admission_handler(commend)
     elif 'club' in commend:
-            if 'cse club' in commend:
-                pt = club_info['cse club']
-                print(pt)
-                talk(pt)
-            elif 'bba club' in commend:
-                pt = club_info['bba club']
-                print(pt)
-                talk(pt)
-            elif 'Pharmacy club' in commend:
-                pt = club_info['Pharmacy club']
-                print(pt)
-                talk(pt)
-            elif ('club' and 'how') in commend:
-                pt = club_info['club']
-                print(pt)
-                talk(pt)
+        club_handler(commend)
     elif 'financial' in commend:
-        pt = club_info['Financial discount']
-        print(pt)
-        talk(pt)
+        financial_handler(commend)
     elif 'lab' in commend:
-        if ('lab' and 'how'):
-            pt = lab_info['lab']
-            print(pt)
-            talk(pt)
+        lab_handler(commend)
     elif 'school' in commend:
-        if ('school' and 'how') in commend:
-            pt = school_info['school']
-            print(pt)
-            talk(pt)
-        elif ('Pharmacy' or 'Public Health' or 'spph') in commend:
-            pt = school_info['spph']
-            print(pt)
-            talk(pt)
-        elif ('sbe' or 'business' or 'entrepreneurship'):
-            pt = school_info['sbe']
-            print(pt)
-            talk(pt)
-        elif ('sets' or 'engineering' or 'technology'):
-            pt = school_info['sets']
-            print(pt)
-            talk(pt)
-        elif ('sels' or 'environment' or 'life sciences'):
-            pt = school_info['sels']
-            print(pt)
-            talk(pt)
-        elif ('slass' or 'liberal' or 'art'):
-            pt = school_info['slass']
-            print(pt)
-            talk(pt)
-        
+        school_handler(commend)
     elif 'stop' in commend:
         exit()
     else:
-        pt = "Please say it again i don't understand"
+        unknown_handler()
+
+def time_handler():
+    time = datetime.datetime.now().strftime('%H:%M %p')
+    print(time)
+    talk('Current time is' + time)
+
+def play_handler(commend):
+    song = commend.replace('play', '')
+    talk("playing" + song)
+    pywhatkit.playonyt(song)
+
+def wikipedia_handler(commend):
+    try:
+        look_for = commend.replace('tell me about','')
+        about = wikipedia.summary(look_for, 2)
+        print(about)
+        talk(about)
+    except:
+        pt = 'Not found in Wikipedia'
         print(pt)
         talk(pt)
+
+def department_handler(commend):
+    if ('department' and 'how') in commend:
+        pt = depermant_info['department']
+        print(pt)
+        talk(pt)
+    elif 'cse department' in commend:
+        pt = depermant_info['cse']
+        print(pt)
+        talk(pt)
+    elif 'bba department' in commend:
+        pt = depermant_info['bba']
+        print(pt)
+        talk(pt)
+    elif 'pharmacy department' in commend:
+        pt = depermant_info['pharmacy']
+        print(pt)
+        talk(pt)
+
+def admission_handler(commend):
+    if 'graduate' in commend:
+        pt = admission_info['graduate']
+        print(pt)
+        talk(pt)
+    else:
+        pt = admission_info['Undergraduate']
+        print(pt)
+        talk(pt)
+
+def financial_handler(commend):
+    pt = club_info['Financial discount']
+    print(pt)
+    talk(pt)
+
+def club_handler(commend):
+    if 'cse club' in commend:
+        pt = club_info['cse club']
+        print(pt)
+        talk(pt)
+    elif 'bba club' in commend:
+        pt = club_info['bba club']
+        print(pt)
+        talk(pt)
+    elif 'Pharmacy club' in commend:
+        pt = club_info['Pharmacy club']
+        print(pt)
+        talk(pt)
+    elif ('club' and 'how') in commend:
+        pt = club_info['club']
+        print(pt)
+        talk(pt)
+
+def school_handler(commend):
+    if ('school' and 'how') in commend:
+        pt = school_info['school']
+        print(pt)
+        talk(pt)
+    elif ('Pharmacy' or 'Public Health' or 'spph') in commend:
+        pt = school_info['spph']
+        print(pt)
+        talk(pt)
+    elif ('sbe' or 'business' or 'entrepreneurship'):
+        pt = school_info['sbe']
+        print(pt)
+        talk(pt)
+    elif ('sets' or 'engineering' or 'technology'):
+        pt = school_info['sets']
+        print(pt)
+        talk(pt)
+    elif ('sels' or 'environment' or 'life sciences'):
+        pt = school_info['sels']
+        print(pt)
+        talk(pt)
+    elif ('slass' or 'liberal' or 'art'):
+        pt = school_info['slass']
+        print(pt)
+        talk(pt)
+
+def lab_handler(commend):
+    if ('lab' and 'how') in commend:
+        pt = lab_info['lab']
+        print(pt)
+        talk(pt)
+
+def unknown_handler():
+    pt = "Please say it again i don't understand"
+    print(pt)
+    talk(pt)
 
 while True:
     run_urro()
